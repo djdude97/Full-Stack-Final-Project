3d45d8ea29a59d6f5763113bcbd802d4247c589e
djdude97 <31464375+djdude97@users.noreply.github.com>
2018-11-30 17:37:56 -0500

    server is now live!

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var exphbs = require('express-handlebars');
-var dataUtil = require("./data-util");
-var _ = require('underscore');
+//var dataUtil = require("./data-util");
+//var _ = require('underscore');
 
 var app = express();
 
@@ -15,10 +15,10 @@ app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
 
 /* Add whatever endpoints you need! Remember that your API endpoints must
- * have '/api' prepended to them. Please remember that you need at least 5 
- * endpoints for the API, and 5 others. 
+ * have '/api' prepended to them. Please remember that you need at least 5
+ * endpoints for the API, and 5 others.
  */
 
 app.listen(3000, function() {
-    console.log('House of Representatives listening on port 3000!');
+    console.log("Drew's Final Project is listening on port 3000!:");
 });
